0e9ecacbb6d47db49728facf49a5131715affce4
prominus <cody.leclair.us@protonmail.com>
2023-12-02 22:22:22 -0500

    Add logo and login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
-    <div id="home-container" class="container py-4 px-3 mx-auto">
-        <h1>Login</h1>
+    <div id="login-container" class="container py-4 px-3 mx-auto">
+        <form class="form-signin">
+                <img src="/src/assets/kd-icon.svg" alt="" width="80" height="80">
+                <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <label for="inputUsername" class="sr-only">Username</label>
+                <input type="text" name="inputUsername" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me">
+                        Remember me    
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+        </form>
     </div>
 </template>
 
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,44 +1,73 @@
 <template>
   <header>
     <div id="nav-container" class="container py-4 px-3 mx-auto">
-      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Koala Down Quest
-          </a>
-          <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <router-link class="nav-link" to="/">Home</router-link>
-              </li>
-              <!-- <li class="nav-item">
-                <router-link class="nav-link" to="/Docs">Docs</router-link>
-              </li> -->
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Documents
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark bg-primary">
-                  <li><router-link class="nav-link" to="/Guide">Guide</router-link></li>
-                  <li><router-link class="nav-link" to="/Links">Links</router-link></li>
-                  <li><router-link class="nav-link" to="/PdfDocs">PDF Docs</router-link></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link bg-danger" href="https://launch.koaladown.quest">Launch Game</a>
-              </li>
-            </ul>
-          </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand mb-0 h1" href="#">
+          <img src="/src/assets/kd-logo.svg" width="120" height="120" class="d-inline-block align-center" alt="">
+          Koala Down Quest
+        </a>
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Documents
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark bg-primary">
+                <li><router-link class="nav-link" to="/Guide">Guide</router-link></li>
+                <li><router-link class="nav-link" to="/Links">Links</router-link></li>
+                <li><router-link class="nav-link" to="/PdfDocs">PDF Docs</router-link></li>
+
+              </ul>
+            </li>
+            <li><a class="nav-link" href="https://launch.koaladown.quest">Launch Game</a></li>
+            <li>
+              <div v-if="isLoggedIn">
+                <button class="btn bg-danger btn-lg text-white my-2 my-sm-0" @click="logout">Logout</button>
+              </div>
+              <div v-else>
+                <form class="form-inline my-2 my-lg-0 ml-auto" @submit.prevent="login">
+                  <div class="input-group">
+                    <input type="text" class="form-control mr-sm-2" placeholder="Username" aria-label="Username">
+                    <input type="password" class="form-control mr-sm-2" placeholder="Password" aria-label="Password"
+                      name="passwdInput" id="passwdInput">
+                    <input type="submit" class="btn bg-danger btn-lg text-white my-2 my-sm-0" value="Login">
+                  </div>
+                </form>
+              </div>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
   </header>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isLoggedIn: false,
+      username: 'Username',
+      password: 'Password'
+    };
+  },
+  methods:
+  {
+    login() {
+      // TODO: authenticate credentials
+      this.isLoggedIn = true;
+
+    },
+    logout() {
+      this.isLoggedIn = false;
+    }
+  }
+}
 </script>
